8cff1abdccee4d778ce599d12e311c0b912b2f93
h.kinoshita <hiroki.kinoshita@kuno-corp.com>
2022-01-17 21:18:38 +0900

    feat: delete hero を追加

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -76,6 +76,14 @@ export class HeroService {
     )
   }
 
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`delete hero id = ${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    )
+  }
+
   /**
    * logger
    * @param message
