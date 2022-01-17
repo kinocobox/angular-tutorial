d648f78f5db971fd45f8b04a59a0bb4f7de70303
h.kinoshita <hiroki.kinoshita@kuno-corp.com>
2022-01-17 19:25:19 +0900

    feat: service の紐付けと非同期処理の仕組みを理解

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { Component, OnInit } from '@angular/core'
+import { Hero } from '../hero'
+import { HeroService } from '../hero.service'
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,15 +9,24 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.sass']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-  selectedHero?: Hero;
+  heroes: Hero[] = []
+  selectedHero?: Hero
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) { }
   ngOnInit(): void {
+    this.getHeros()
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+  }
+
+  getHeros(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
   }
 
 }
